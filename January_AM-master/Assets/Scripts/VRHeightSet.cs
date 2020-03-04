65080f3ed34a12176da4a800c635f3b0639744ba
cyb3r6 <45832126+cyb3r6@users.noreply.github.com>
2020-03-04 12:51:10 -0700

    Session 11

---
+++ January_AM-master/Assets/Scripts/VRHeightSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.SpatialTracking;
+
+public class VRHeightSet : MonoBehaviour
+{
+    //Set it to floor level
+    private void Awake()
+    {
+        XRDevice.SetTrackingSpaceType(TrackingSpaceType.RoomScale);
+    }
+}
--- January_AM-master/Assets/Scripts/VoiceCommander.cs
+++ January_AM-master/Assets/Scripts/VoiceCommander.cs
@@ -8,18 +8,50 @@ using UnityEngine.Windows.Speech;   //To use speech Recognition
 
 public class VoiceCommander : MonoBehaviour
 {
-
     private Dictionary<string, Action> m_keyWordActions = new Dictionary<string, Action>();
 
-    // Start is called before the first frame update
-    void Start()
+    private KeywordRecognizer m_keywordRecognizer;
+
+    private void Awake()
     {
-        
+        m_keyWordActions.Add("slap dale", SlapDale); //You phrase and then the function you want to call
+        m_keyWordActions.Add("Ra za na ba do a", SlapDale);
+        m_keyWordActions.Add("Spawn dale", SpawnDale);
+        m_keyWordActions.Add("Spawn cube", SpawnCube);
+        m_keyWordActions.Add("Spawn box", SpawnCube);
+
+        m_keywordRecognizer = new KeywordRecognizer(m_keyWordActions.Keys.ToArray());
+        m_keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognized;
+        m_keywordRecognizer.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnKeywordsRecognized(PhraseRecognizedEventArgs args)
     {
-        
+        m_keyWordActions[args.text].Invoke();
+    }
+    
+    //The functions you call
+    void SlapDale()
+    {
+        Debug.Log("How dare you! Dale is so cool! Stop being so mean!");
+    }
+
+    [SerializeField]
+    private GameObject m_prefabDale;
+
+    [SerializeField]
+    private Transform m_SpawnPosition;
+
+    void SpawnDale()
+    {
+        Instantiate(m_prefabDale, m_SpawnPosition.position, m_SpawnPosition.rotation);
+    }
+
+    [SerializeField]
+    private GameObject m_cube;
+
+    void SpawnCube()
+    {
+        Instantiate(m_cube, m_SpawnPosition.position, m_SpawnPosition.rotation);
     }
 }
