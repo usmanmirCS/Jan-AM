8b0b5e6b9a06f9c3f12f02e694c31013eb3f54eb
cyb3r6 <45832126+cyb3r6@users.noreply.github.com>
2020-03-23 12:54:36 -0600

    Session 16

---
+++ January_AM-master/Assets/Scripts/AR/VuforiaARInstructions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VuforiaARInstructions : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_mainMenuCanvas;
+    [SerializeField]
+    private GameObject m_thisCanvas;
+
+    [SerializeField]
+    private List<GameObject> m_steps = new List<GameObject>();
+
+    private int m_currentStep = 0;
+
+    public void StepsController()
+    {
+        if(m_currentStep < m_steps.Count - 1)
+        {
+            m_steps[m_currentStep].SetActive(false);
+            m_currentStep++;
+            m_steps[m_currentStep].SetActive(true);
+        }
+        else if(m_currentStep == m_steps.Count - 1)
+        {
+            m_steps[m_currentStep].SetActive(false);
+            m_currentStep = 0;
+            m_steps[m_currentStep].SetActive(true);
+            m_mainMenuCanvas.SetActive(true);
+            m_thisCanvas.SetActive(false);
+        }
+    }
+}
