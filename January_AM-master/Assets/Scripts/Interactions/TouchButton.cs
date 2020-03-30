b1a8b87bbbba6d1a7f865f6ca1f4466e1209c892
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-03-30 12:45:07 -0600

    Session 18

---
+++ January_AM-master/Assets/Scripts/Interactions/TouchButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class TouchButton : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_upTransform;
+    [SerializeField]
+    Transform m_downTransform;
+    [SerializeField]
+    Transform m_buttonMesh;
+
+    [SerializeField]
+    UnityEvent m_buttonPressed;
+    [SerializeField]
+    UnityEvent m_buttonReleased;
+
+    Vector3 m_startPosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //m_buttonMesh.position = m_downTransform.position;
+        m_buttonPressed.Invoke();
+
+        m_startPosition = other.transform.position;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_buttonMesh.position = m_upTransform.position;
+        m_buttonReleased.Invoke();
+    }
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        float deltaPosition = m_startPosition.z - transform.position.z;
+
+        m_buttonMesh.position = new Vector3(m_buttonMesh.position.x, m_buttonMesh.position.y, deltaPosition);
+    }
+}
